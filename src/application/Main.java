cb6bbd8406762106f9f2ea834e7b853454bac60a
Escapist <daydreamer.1007@gmail.com>
2017-10-17 17:02:10 +0700

    fixed typo for MainWindow.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MianWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         primaryStage.setTitle("Ciburuy Desk");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
