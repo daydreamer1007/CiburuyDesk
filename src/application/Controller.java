38512c5c8d62bca39714209af7e786b9e965d4fe
Escapist <daydreamer.1007@gmail.com>
2017-11-20 20:18:49 +0700

    Added jdbc library (for mysql connection), not used yet.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import java.io.IOException;
+import java.sql.*;
 import java.util.List;
 
 public class Controller {
